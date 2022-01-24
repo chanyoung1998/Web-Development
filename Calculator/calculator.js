b6f267f748c8163560f012803fa8931ad290059e
CHAN YOUNG <lms062111@naver.com>
2022-01-24 21:25:07 +0900

    studying node js/ express/ responding to requests with html files

--- Calculator/calculator.js
+++ Calculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require('express')
+const bodyParser = require("body-parser");
 const app = express()
 const port = 3000
+app.use(bodyParser.urlencoded({extended:true}));
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.listen(port);
+app.post('/',function(req,res){
+  var num1 = Number(req.body.num1);
+  var num2 = Number(req.body.num2);
+  var result = num1 + num2;
+
+  res.send("Thanks for posting:" + result);
+});
+
+app.listen(port,function(){
+  console.log('Server is running on port 3000');
+});
